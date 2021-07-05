a4fdcfad6bdfee45c922918ee7385dcea536c2f1
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-05 19:03:42 +0200

    Doc erstellt

--- db/Adresse.java
+++ db/Adresse.java
@@ -106,39 +106,155 @@ public class Adresse {
 	private void change_address_hn(int adress_id,String hn)throws ClassNotFoundException, SQLException{
 		change_db_value_for_address("UPDATE adresse SET hn = '"+hn+"' WHERE adress_id = '"+adress_id+"'");
 	}
+	/**
+	 * Diese Methode erstellt einen neuen Datenbankeintrag.
+	 * @param plz Die Postleitzahl des Ortes als String.
+	 * @param ort	Den Ortsnamen als String.
+	 * @param strasse Die Straße als String.
+	 * @param hn	Die Hausnummer als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void set_address_value(String plz,String ort, String strasse, String hn)throws ClassNotFoundException, SQLException{
 		set_db_value_for_address(plz,ort,strasse,hn);
 	}
+	/**
+	 * Diese Methode gibt die Adress-ID zurück
+	 * @param plz	Die Postleitzahl als String.
+	 * @param ort	Den Ort als String.
+	 * @param strasse	Die Straße als String.
+	 * @param hn	Die Hausnummer als String.
+	 * @return	Die Address-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int get_adress_id(String plz,String ort, String strasse, String hn)throws ClassNotFoundException, SQLException{
 		return get_address_adress_id(plz,ort,strasse,hn);
 	}
+	/**
+	 * Diese Methode gibt die Postleizahl zurück.
+	 * @param adress_id Die Adress-ID als Integer
+	 * @return Die Postleizahl als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_plz(int adress_id)throws ClassNotFoundException, SQLException{
 		return get_address_plz(adress_id);
 	}
+	/**
+	 * Diese Methode gibt den Ort zurück.
+	 * @param adress_id Die Adress-ID als Integer
+	 * @return Den Ort als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_ort(int adress_id)throws ClassNotFoundException, SQLException{
 		return get_address_ort(adress_id);
 	}
+	/**
+	 * Diese Methode gibt die Straße zurück.
+	 * @param adress_id Die Adress-ID als Integer.
+	 * @return Die Straße als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_strasse(int adress_id)throws ClassNotFoundException, SQLException{
 		return get_address_strasse(adress_id);
 	}
+	/**
+	 * Diese Methode gibt die Hausnummer zurück.
+	 * @param adress_id Die Adress-ID als Integer
+	 * @return Die Hausnummer als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_hn(int adress_id)throws ClassNotFoundException, SQLException{
 		return get_address_hn(adress_id);
 	}
+	/**
+	 * Diese Methode gibt alle Datenbankeinträge aus.
+	 * <p> Es werden keine Parameter übergeben </p>
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void display_adresse()throws ClassNotFoundException, SQLException{
 		display_address_all();
 	}
+	/**
+	 * Diese Methode ändert die Postleitzahl.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param adress_id Die Adress-ID als Integer
+	 * @param plz Die neue Postleitzahl als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_plz(int adress_id, String plz)throws ClassNotFoundException, SQLException{
 		change_address_plz(adress_id,plz);
 	}
+	/**
+	 * Diese Methode ändert den Ort.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param adress_id Die Adress-ID als Integer
+	 * @param ort Den neuen Ort als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_ort(int adress_id, String ort)throws ClassNotFoundException, SQLException{
 		change_address_ort(adress_id,ort);
 	}
+	/**
+	 * Diese Methode ändert die Straße.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param adress_id Die Adress-ID als Integer.
+	 * @param strasse Die neue Straße als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_strasse(int adress_id, String strasse)throws ClassNotFoundException, SQLException{
 		change_address_strasse(adress_id,strasse);
 	}
+	/**
+	 * Diese Methode ändert die Hausnummer.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param adress_id Die Adress-ID als Intger.
+	 * @param hn Die neue Hausnummer als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_hn(int adress_id, String hn)throws ClassNotFoundException, SQLException{
 		change_address_hn(adress_id,hn);
 	}
+	/**
+	 * Diese Methode löscht eine Adresse.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param id Die Adress-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void delete_addresse(int id) throws ClassNotFoundException, SQLException{
 		delete_db_value(id);
 	}
