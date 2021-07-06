43f7873da1dccf914e662f1074e08fbb9a4c5b1b
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-06 13:51:14 +0200

    initiert die Tabelle

--- db/Wohnung.java
+++ db/Wohnung.java
@@ -1,4 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Wohnung {
-
+	public Wohnung() throws ClassNotFoundException, SQLException{
+		initieren();
+	}
+	private void initieren() throws ClassNotFoundException, SQLException{
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:sqlite:kundenDB.db");//stelle verbindung zur DB her
+		Statement statement = connection.createStatement();
+		statement.setQueryTimeout(30);
+		statement.executeUpdate("CREATE TABLE IF NOT EXISTS wohnung(wohnungs_id INTEGER PRIMARY KEY, adress_id INTEGER NOT NULL, miete DOUBLE,zimmer DOUBLE NOT NULL,baeder DOUBLE NOT NULL,ebk BOOLEAN)");
+		connection.close();
+	}
 }
