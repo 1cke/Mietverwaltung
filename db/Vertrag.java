1c0a7289c0ec66a917887193d84ef9e6f9ef7442
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-07 21:49:06 +0200

    Doc erstellt

--- db/Vertrag.java
+++ db/Vertrag.java
@@ -128,30 +128,106 @@ public class Vertrag {
 		String id = get_values("SELECT rowid FROM vertrag ORDER BY rowid DESC limit 1")[0];
 		return Integer.parseInt(id);
 	}
-	
+	/**
+	 * Diese Methode legt einen Vertrag an.
+	 * @param kd_id Die Kunden-ID als Integer.
+	 * @param whg_id Die Wohnungs-ID als Integer.
+	 * @param schulden	Die Höhe der Schulden als Double.
+	 * @param zeitraum	Den Zeitraum als String.
+	 * @param aktiv	Den Status als Integer. 0 = False, 1 = True.
+	 * @return Die ID des Vertrages als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int set_db_value(int kd_id,int whg_id,double schulden, String zeitraum,int aktiv)throws ClassNotFoundException, SQLException{
 		set_db_value_for_contract(kd_id,whg_id,schulden,zeitraum,aktiv);
 		int id = get_last_id();
 		return id;
 	}
+	/**
+	 * Diese Methode gibt anhand der Kunden-ID die Vertrags-ID zurück.
+	 * @param kd_id Die Kunden-ID als Integer.
+	 * @return DIe Vertrags-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int get_vertrags_id(int kd_id) throws ClassNotFoundException, SQLException{
 		return get_vertrag_id(kd_id);
 	}
+	/**
+	 * Diese Methode gibt die zugeordnete Kunden-ID zurück.
+	 * @param vertrags_id Die Vertrags-ID als Integer.
+	 * @return Die Kunden-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int get_kd_id(int vertrags_id) throws ClassNotFoundException, SQLException {
 		return get_vertrag_kd_id(vertrags_id);
 	}
+	/**
+	 * Diese Methode gibt die zugeordnete Wohnungs-ID zurück.
+	 * @param vertrags_id Die Vertrags-ID als Integer.
+	 * @return DIe Wohnungs-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int get_whg_id(int vertrags_id) throws ClassNotFoundException, SQLException{
 		return get_vertrag_whg_id(vertrags_id);
 	}
+	/**
+	 * Diese Methode gibt den Zeitraum zurück.
+	 * <p> Im sinne von aktiv seit oder war Kunde von xy bis yz.
+	 * @param vertrags_id Die Vertrags-ID als Integer.
+	 * @return Den Zeitraum als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_zeitraum(int vertrags_id) throws ClassNotFoundException, SQLException{
 		return get_vertrag_zeitraum(vertrags_id);
 	}
+	/**
+	 * Diese Methode gibt die Höhe der Schulden zurück.
+	 * @param vertrags_id Die Vertrags-ID als Integer.
+	 * @return Die Schulden als Double. 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public double get_schulden(int vertrags_id)throws ClassNotFoundException, SQLException{
 		return get_vertrag_schulden(vertrags_id);
 	}
+	/**
+	 * Diese Methode gibt den Vertragsstatus zurück.
+	 * @param vertrags_id Die Vertrags-ID als Integer.
+	 * @return	Den Status als Boolean.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public boolean get_aktiv(int vertrags_id)throws ClassNotFoundException, SQLException{
 		return get_vertrag_aktiv(vertrags_id);
 	}
+	/**
+	 * Diese Methode gibt alle Verträge von einem Kunden zurück.
+	 * @param kd_nr Die Kunden-ID als Integer.
+	 * @return	Ein Integerarray mit allen zugeordneten Vertrags-IDs.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int[] get_all_whg(int kd_nr) throws ClassNotFoundException, SQLException{
 		/*
 		 * rückgabe als array muss im Code dann einzeln abgerufen werden
@@ -167,21 +243,78 @@ public class Vertrag {
 		}
 		return wert;
 	}
+	/**
+	 * Diese Methode gibt alle Verträge und dazugehörige Daten aus
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void get_all()throws ClassNotFoundException, SQLException{
 		get_vertrag_all();
 	}
+	/**
+	 * Diese Methode ändert die Kunden-ID.
+	 * @param id	Die Vertrags-ID als Integer.
+	 * @param kd_nr	Die neue Kunden-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_kundennummer(int id, int kd_nr)throws ClassNotFoundException, SQLException {
 		change_vertrag_kd_id(id,kd_nr);
 	}
+	/**
+	 * Diese Methode ändert die Wohnungs-ID.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param id	Die Vertrags-ID als Integer.
+	 * @param wohnungsnummer	Die neue Wohnungs-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_wohnungsnummer(int id, int wohnungsnummer) throws ClassNotFoundException, SQLException{
 		change_vertrag_whg_id(id,wohnungsnummer);
 	}
+	/**
+	 * Diese Methode ändert den Zeitraum.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param id Die Vertrags-ID als Integer.
+	 * @param zeitraum Den neuen Zeitraum als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_zeitraum(int id,String zeitraum)throws ClassNotFoundException, SQLException{
 		change_vertrag_zeitraum(id,zeitraum);
 	}
+	/**
+	 * Diese Methode ändert den Schuldenwert.
+	 * <p> Es erfolgt keine Rückgabe</p>
+	 * @param id Die Vertrags-ID als Integer.
+	 * @param schulden Die neuen Schulden als Double
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_schulden(int id, double schulden)throws ClassNotFoundException, SQLException{
 		change_vertrag_schulden(id,schulden);
 	}
+	/**
+	 * Diese Methode ändert den aktiv Status.
+	 * <p>Es erfolgt keine Rückgabe</p>
+	 * @param id Die Vertrags-Id als Integer.
+	 * @param aktiv	Den neuen Status als Integer. 0 = false, 1 = True
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_aktiv(int id, int aktiv)throws ClassNotFoundException, SQLException{
 		change_vertrag_aktiv(id,aktiv);
 	}
