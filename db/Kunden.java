cd4c336739237c06e2893bdca454b0f00efdcb6d
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-07 22:47:58 +0200

    Doc erstellt

--- db/Kunden.java
+++ db/Kunden.java
@@ -148,51 +148,194 @@ public class Kunden {
 	private int get_last_id()throws ClassNotFoundException, SQLException{
 		return Integer.parseInt(get_values("SELECT rowid FROM kunden ORDER BY rowid DESC limit 1")[0]);
 	}
-	
+	/**
+	 * Diese Methode erstellt einen Kunden.
+	 * <p> Der Kunde wird Standardmäßig aktiv gesetzt</p>
+	 * @param vorname Den Vornamen des Kunden als String.
+	 * @param nachname Den Nachnamen des Kunden als String.
+	 * @param geburtstag Das Geburtsdatum als String.
+	 * @param telefon Die Telefonnummer als String. Falls unbekannt, leeren String " ".
+	 * @param email Die Email als String. Falls unbekannt, leeren String " ".
+	 * @param interessent Den Interessentenstatus des Kunden als Integer. 0 = False, 1 = True.
+	 * @return Die ID des Kunden als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int set_db_value(String vorname, String nachname, String geburtstag, String telefon,String email,int interessent)throws ClassNotFoundException, SQLException {
 		set_db_value_for_kd(vorname,nachname,geburtstag,telefon,email,interessent);
 		int id = get_last_id();
 		return id;
 	}
+	/**
+	 * Diese Methode gibt die Kunden-ID zurück.
+	 * <p> Alle Parameter müssen so geschrieben werden, wie Sie in der Datenbank stehen. Ansonsten gibt es keine Ausgabe.</p>
+	 * <p> Dieser Zwang wird noch geändert</p>
+	 * @param vorname Den Vornamen des Kunden als String.
+	 * @param nachname Den Nachnamen des Kunden als String.
+	 * @param geburtstag Das Geburtsdatum des Kunden als String.
+	 * @return Die Kunden-ID als Integer.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public int get_id(String vorname, String nachname, String geburtstag)throws ClassNotFoundException, SQLException {
 		return get_kd_id(vorname,nachname, geburtstag);
 	}
+	/**
+	 * Diese Methode gibt den Vornamen des Kunden zurück.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Den Vornamen als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_vorname(int id) throws ClassNotFoundException, SQLException{
 		return get_kd_vorname(id);
 	}
+	/**
+	 * Diese Methode gibt den Nachnamen des Kunden zurück.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Den Nachnamen als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_nachname(int id) throws ClassNotFoundException, SQLException{
 		return get_kd_nachname(id);
 	}
+	/**
+	 * Diese Methode gibt das Geburtsdatum des Kunden zurück.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Das Geburtsdatum als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_geburtstag(int id) throws ClassNotFoundException, SQLException{
 		return get_kd_geburtstag(id);
 	}
+	/**
+	 * Diese Methode gibt die Telefonnummer des Kunden zurück.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Die Telefonnummer als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_telefon(int id) throws ClassNotFoundException, SQLException{
 		return get_kd_telefon(id);
 	}
+	/**
+	 * Diese Methode gibt die Email des Kunden zurück.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Die Email als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public String get_email(int id)throws ClassNotFoundException, SQLException{
 		return get_kd_email(id);
 	}
+	/**
+	 * Diese Methode gibt zurück ob der Kunde ein Interessent ist.
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Den Interessentenstatus als Boolean.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public boolean get_interessent(int id)throws ClassNotFoundException, SQLException{
 		return get_kd_interessent(id);
 	}
+	/**
+	 * Diese Methode gibt den Status des Kunden zurück.
+	 * <p> Ist dieser noch Kunde oder nicht</p>
+	 * @param id Die Kunden-ID als Integer.
+	 * @return Den Status als Boolean.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public boolean get_aktiv(int id)throws ClassNotFoundException, SQLException{
 		return get_kd_aktiv(id);
 	}
+	/**
+	 * Diese Methode ändert den Vornamen des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param vorname Den Vornamen als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_vorname(int id, String vorname) throws ClassNotFoundException, SQLException{
 		change_kd_vorname(id,vorname);
 	}
+	/**
+	 * Diese Methode ändert den Nachnamen des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param nachname Den Nachnamen als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_nachname(int id, String nachname)throws ClassNotFoundException, SQLException{
 		change_kd_nachname(id,nachname);
 	}
+	/**
+	 * Diese Methode ändert das Geburtsdatum des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param geburtstag Das Geburtsdatum als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_geburtstag(int id, String geburtstag) throws ClassNotFoundException, SQLException{
 		change_kd_geburtstag(id,geburtstag);
 	}
+	/**
+	 * Diese Methode ändert die Telefonnummer des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param telefon Die Telefonnummer als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_telefon(int id, String telefon) throws ClassNotFoundException, SQLException{
 		change_kd_telefon(id,telefon);
 	}
+	/**
+	 * Diese Methode ändert die Email des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param email Die Email als String.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_email(int id, String email) throws ClassNotFoundException, SQLException{
 		change_kd_email(id,email);
 	}
+	/**
+	 * Diese Methode ändert den Interessentenstatus des Kunden.
+	 * @param id Die Kunden-ID als Integer.
+	 * @param interessent Den Interessentenstatus als Integer. 0 = False, 1 = True.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public void change_interessent(int id, int interessent) throws ClassNotFoundException, SQLException{
 		change_kd_interessent(id,interessent);
 	}
