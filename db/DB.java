5ca21b4844c92092fb8bdd3b5a05fcae9d275d7b
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-09 13:31:45 +0200

    Test durchgeführt

--- db/DB.java
+++ db/DB.java
@@ -18,24 +18,24 @@ public class DB {
 		
 		//adresse.set_address_value("10117", "Berlin", "Pariser Platz", "2");
 		//adresse.set_address_value("20457", "Hamburg", "Platz der Deutschen Einheit", "4");
-		//adresse.set_address_value("80939", "München", "Werner-Heisenberg-Alle", "25");
-		//whg.set_wohnungen(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2"), 527.43, 3, 1, 1, 1);
+		//adresse.set_address_value("80939", "München", "Werner-Heisenberg-Allee", "25");
+		//whg.set_wohnungen(adresse.get_adress_id("80939", "München", "Werner-Heisenberg-Allee", "25"), 638.72, 4, 2, 1, 1);
 		//whg.set_wohnungen(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2"), 189.52, 1, 1, 1, 1);
 		//whg.set_wohnungen(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2"), 1052.00, 5, 3.5, 1, 1);
 		//whg.set_wohnungen(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2"), 802.41, 4, 2,1, 1);
 		//whg.display();
 		//kunde.set_db_value("Max", "Mustermann", "01.01.2001", "+4930/2345619", " ",1);
 		//kunde.set_db_value("Hannelore", "Müller", "23.04.1982", " ", "hannelore_mueller@test.de",1);
-		//kunde.set_db_value("Peter", "Lustig", "01.01.1972", "+49157/32568792", "peter.lustig@test.de",0);
+		//kunde.set_db_value("Franzl", "Wolf", "13.05.1968", " ", "franzl.wolf@test.de",0);
 		//kunde.get_all();
 		//adresse.display_adresse();
-		//vertrag.set_db_value(kunde.get_id("Hannelore", "Müller", "23.04.1982"), whg.get_whg_nr(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2")), 0.0,"01.07.2021", 1);
+		//vertrag.set_db_value(kunde.get_id("Franzl", "Wolf", "13.05.1968"), whg.get_whg_nr(adresse.get_adress_id("80939", "München", "Werner-Heisenberg-Allee", "25")), 0.0,"05.02.2002", 1);
 		//vertrag.set_db_value(kunde.get_id("Max", "Mustermann", "01.01.2001"), whg.get_whg_nr(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2")), 20.0,"01.01.2011", 1);
 		//vertrag.set_db_value(kunde.get_id("Peter", "Lustig", "01.01.1972"), whg.get_whg_nr(adresse.get_adress_id("10117", "Berlin", "Pariser Platz", "2")), 20.0,"01.01.2011", 0);
-		//kontakt.set_kontakt_value(vertrag.get_vertrags_id(kunde.get_id("Max", "Mustermann", "01.01.2001")), "06.07.2021", "Wasserschaden", "Es tropft aus der Decke");
+		//kontakt.set_kontakt_value(vertrag.get_vertrags_id(kunde.get_id("Franzl", "Wolf", "13.05.1968")), "27.09.2011", "Rohrbruch", " ");
 		System.out.println("Test ausgabe");
 		
-		int kd_nr = kunde.get_id("Hannelore", "Müller", "23.04.1982");
+		int kd_nr = kunde.get_id("Franzl", "Wolf", "13.05.1968");
 		String vorname = kunde.get_vorname(kd_nr);
 		String nachname = kunde.get_nachname(kd_nr);
 		String geburtsdatum = kunde.get_geburtstag(kd_nr);
