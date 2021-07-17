6c1cdb96608a38bb7cf990ba712811d8f5c70021
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-17 14:23:20 +0200

    neuer Datentyp

---
+++ db/datentypen/Anschrift.java
@@ -0,0 +1,74 @@
+package datentypen;
+
+public class Anschrift {
+	private int id = 0;
+	private String plz = "";
+	private String ort = "";
+	private String strasse = "";
+	private String hausnummer = "";
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Anschrift [id=" + id + ", plz=" + plz + ", ort=" + ort + ", strasse=" + strasse + ", hausnummer="
+				+ hausnummer + "]";
+	}
+	/**
+	 * @return the plz
+	 */
+	public String getPlz() {
+		return plz;
+	}
+	/**
+	 * @param plz the plz to set
+	 */
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+	/**
+	 * @return the ort
+	 */
+	public String getOrt() {
+		return ort;
+	}
+	/**
+	 * @param ort the ort to set
+	 */
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	/**
+	 * @return the strasse
+	 */
+	public String getStrasse() {
+		return strasse;
+	}
+	/**
+	 * @param strasse the strasse to set
+	 */
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	/**
+	 * @return the hausnummer
+	 */
+	public String getHausnummer() {
+		return hausnummer;
+	}
+	/**
+	 * @param hausnummer the hausnummer to set
+	 */
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+}
