93bd0770a3c3dafdfe3423564badc73d7ce39c5f
Konrad Wielenski <konrad.wielenski@gmx.de>
2021-07-19 17:38:25 +0200

    message

--- db/GUIs/Bewerbung_Level2.java
+++ db/GUIs/Bewerbung_Level2.java
@@ -20,7 +20,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Bewerbung_Level2 extends JFrame {
-
+//tets
 	private JPanel contentPaneB2;
 	private JTextField txtFKundenIDB2;
 	private JTextField txtFWohnungsIDB2;
