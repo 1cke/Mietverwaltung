0788930a3fca0460782277e935214c14a4837ffc
johannmunchhagen <johannmunchhagen@johanns-mbp.fritz.box>
2021-07-29 10:33:21 +0200

    Doc angepasst

--- db/backend/Kontakt.java
+++ db/backend/Kontakt.java
@@ -13,13 +13,31 @@ import java.util.ArrayList;
 import datentypen.Kontaktdaten;
 
 /**
+ * Diese Klasse stellt Daten zu den Kontakten bereit
  * @author Johann-Christian Münchhagen
- *
+ * @version 1.0
+ * @see Kontaktdaten
  */
 public class Kontakt extends Kontaktdaten{
+	/**
+	 * Diese Methode konstruiert die Klasse
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see initieren
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	public Kontakt() throws ClassNotFoundException, SQLException{
 		initieren();
 	}
+	/**
+	 * Diese Methode überprüft ob die Tabelle kontakt bereits in der Datenbank existiert
+	 * Wenn dies nicht der Fall ist, wird diese Tabelle neu angelegt
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private void initieren()throws ClassNotFoundException, SQLException {
 		Connection connection = null;
 		connection = DriverManager.getConnection("jdbc:sqlite:kundenDB.db");//stelle verbindung zur DB her
@@ -28,6 +46,15 @@ public class Kontakt extends Kontaktdaten{
 		statement.executeUpdate("CREATE TABLE IF NOT EXISTS kontakt(kontakts_id INTEGER PRIMARY KEY, vertrags_id INTEGER NOT NULL, wann TEXT NOT NULL,grund TEXT NOT NULL,beschreibung TEXT,behoben BOOLEAN)");
 		connection.close();
 	}
+	/**
+	 * Diese Methode erstellt einen neuen Datenbank eintrag
+	 * @param anweisung EIín SQL-Befehl als String
+	 * @return Boolean als Funktionsindikator
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean set_db_value_for_contact(String anweisung)throws ClassNotFoundException, SQLException{
 		try {
 			Connection connection = null;//setze Connection auf null
@@ -43,6 +70,16 @@ public class Kontakt extends Kontaktdaten{
 		}
 		
 	}
+	/**
+	 * Diese Methode lädt einen spezifischen Kontakt
+	 * @param kontakts_id Die Kontakts-ID als Integer
+	 * @return Boolean als Funktionsindikator
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean get_values(int kontakts_id) throws ClassNotFoundException, SQLException{
 		try {
 			Connection connection = null;
@@ -66,6 +103,17 @@ public class Kontakt extends Kontaktdaten{
 		}
 		
 	} 
+	/**
+	 * Diese Methode lädt alle Kontakte die einem Vertrag zugeordnet werden können, bzw lädt diese Methode alle Kontakte
+	 * @param vertrags_id Die Vertrags-ID als Integer
+	 * @param typ den Typ als String
+	 * @return ArrayList vom Typ Kontaktdaten
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private ArrayList<Kontaktdaten> get_values(int vertrags_id,String typ) throws ClassNotFoundException, SQLException{
 		ArrayList<Kontaktdaten> daten = new ArrayList<Kontaktdaten>();
 		Connection connection = null;
@@ -103,7 +151,16 @@ public class Kontakt extends Kontaktdaten{
 			return daten;
 		}
 	
-	} 
+	}
+	/**
+	 * Diese Methode ändert ein Attribut in der Datenbank
+	 * @param anweisung Ein SQL-Befehl als String
+	 * @return Boolean als Funktionsindikator
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean change_db_value(String anweisung)throws ClassNotFoundException, SQLException{
 		try {
 			Connection connection = null;//setze Connection auf null
@@ -119,27 +176,103 @@ public class Kontakt extends Kontaktdaten{
 		}
 		
 	}
+	/**
+	 * Diese Methode gibts die Vertrags-ID zurück
+	 * @return Die Vertrags-ID als Integer
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private int get_contact_vertrags_id()throws ClassNotFoundException, SQLException{
 		return this.getVertragsnummer();
 	}
+	/**
+	 * Diese Methode gibt das Datum zurück
+	 * @return Das Datum als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private String get_contact_wann()throws ClassNotFoundException, SQLException{
 		return this.getWann();
 	}
+	/**
+	 * Diese Methode gibt den Grund zurück
+	 * @return Den Grund als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private String get_contact_grund()throws ClassNotFoundException, SQLException{
 		return this.getGrund();
 	}
+	/**
+	 * Diese Methode gibt die Beschreibung zurück
+	 * @return Die Beschreibung als String
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private String get_contact_beschreibung()throws ClassNotFoundException, SQLException{
 		return this.getBeschreibung();
 	}
+	/**
+	 * Diese Methode gibt den Behobenstatus zurück
+	 * @return Der Status als Boolean
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean get_contact_behoben()throws ClassNotFoundException, SQLException{
 		return this.isBehoben();
 	}
+	/**
+	 * Diese Methode ändert die Beschreibung
+	 * @param kontakt_id Die Kontakt-ID als Integer
+	 * @param beschreibung Die neue Beschreibung als String
+	 * @return Boolean als Funktionsindikator
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see change_db_value
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean change_contact_beschreibung(int kontakt_id,String beschreibung)throws ClassNotFoundException, SQLException{
 		return change_db_value("UPDATE kontakt SET beschreibung = '"+beschreibung+"' WHERE kontakts_id = '"+kontakt_id+"'");
 	}
+	/**
+	 * Diese Methode ändert den Behobenstatus
+	 * @param kontakt_id Die Kontakt-ID als Integer
+	 * @param behoben Den neuen Status als Integer. 1 = True, 0 = False
+	 * @return Boolean als Funktionsindikator
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see change_db_value
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private boolean change_contact_behoben(int kontakt_id,int behoben)throws ClassNotFoundException, SQLException{
 		return change_db_value("UPDATE kontakt SET behoben = '"+behoben+"' WHERE kontakts_id = '"+kontakt_id+"'");
 	}
+	/**
+	 * Diese Method gibt alle Kontakte zurück
+	 * @return ArrayList vom Typ Kontaktdaten
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
+	 */
 	private ArrayList<Kontaktdaten> get_display_contact() throws ClassNotFoundException, SQLException{
 		return get_values(0,"");
 	}
@@ -152,6 +285,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Boolean
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see set_db_value_for_contract
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -165,6 +299,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Die Vertrags-ID als Integer.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_contract_vertrags_id
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -176,6 +311,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Den Zeitpunkt als String.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_contact_wann
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -187,6 +323,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Den Grund als String.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_contact_grund
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -198,6 +335,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Die Beschreibung als String.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_contact_beschreibung
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -209,6 +347,9 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Den Status als Boolean.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_contact_behoben
+	 * @see ClassNotFoundException
+	 * @see SQLException
 	 */
 	public boolean get_behoben()throws ClassNotFoundException, SQLException{
 		return get_contact_behoben();
@@ -221,6 +362,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Boolean
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see change_contact_beschreibung
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -234,6 +376,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Boolean
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see change_contact_behoben
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -245,6 +388,8 @@ public class Kontakt extends Kontaktdaten{
 	 * @return ArrayList vom Typ Kontaktdaten
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_display_contact
+	 * @see Kontaktdaten
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -257,6 +402,7 @@ public class Kontakt extends Kontaktdaten{
 	 * @return Boolean.
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_values
 	 * @see ClassNotFoundException
 	 * @see SQLException
 	 */
@@ -269,6 +415,10 @@ public class Kontakt extends Kontaktdaten{
 	 * @return eine ArrayList vom Typ Kontaktdaten
 	 * @throws ClassNotFoundException
 	 * @throws SQLException
+	 * @see get_values
+	 * @see Kontaktdaten
+	 * @see ClassNotFoundException
+	 * @see SQLException
 	 */
 	public ArrayList<Kontaktdaten>get_all_fuer_einen_vertrag(int vertrags_id)throws ClassNotFoundException, SQLException{
 		return get_values(vertrags_id,"einer");
